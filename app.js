439c42ac6085e5f93fa1ad0c2c72238487b6a8b1
John15j <83560505+John15j@users.noreply.github.com>
2026-06-07 14:29:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+/* ================================================= */
+/* ERLC JUDICIAL SYSTEM */
+/* PHASE 1 */
+/* ================================================= */
 
+/* CLOCK */
+
+function updateClock(){
+
+    const clock =
+    document.getElementById("clock");
+
+    if(!clock) return;
+
+    clock.innerText =
+    new Date().toLocaleTimeString();
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
